38cb3b6502d4df373b80697f9caa6653c4d45497
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-09-13 14:41:05 +0200

    [add]: HTML layout

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// 1. creo un array con le immagini
+// 2. stampo le immagini dell'array dentro l'html con classe d-none (tranne il primo a cui tolgo il d-none)
+// 3. creo i bottoni con i quali modifico la classe d-none delle immagini
+// 4. assegno la possibilita ai bottoni di resettare lo scorrimento delle immagini per ricominciare a vedere le immagini
+
+
+
